3ced63bc79171203fdab069b1f3eaf87b3a616d9
Patrik Svensson <patrik@patriksvensson.se>
2018-08-15 22:52:02 +0200

    Fixes test layout.
    Should make it easier to maintain and add new tests.
    
    Closes #7

---
+++ src/Spectre.Query.Tests/Infrastructure/TestQueryRunner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Spectre.Query.Tests.Data;
+
+namespace Spectre.Query.Tests.Infrastructure
+{
+    public static class TestQueryRunner
+    {
+        public static async Task<TestQueryRunnerResult> Execute(string query, Func<List<Invoice>> seeder, Action<IQueryConfigurator<DataContext>> options = null)
+        {
+            seeder = seeder ?? throw new InvalidOperationException(nameof(seeder));
+            options = options ?? ConfigureDefaultQueryProvider;
+
+            using (var connection = new SqliteConnection("DataSource=:memory:"))
+            {
+                connection.Open();
+
+                var builder = new DbContextOptionsBuilder<DataContext>().UseSqlite(connection);
+                using (var context = new DataContext(builder.Options))
+                {
+                    context.Database.EnsureCreated();
+
+                    // Seed
+                    await context.Invoices.AddRangeAsync(seeder());
+                    await context.SaveChangesAsync();
+
+                    // Initialize provider.
+                    var provider = QueryProviderBuilder.Build(context, options);
+
+                    // Execute query.
+                    var result = provider.Query<Invoice>(context, query).ToList();
+                    return new TestQueryRunnerResult
+                    {
+                        Query = query,
+                        Result = result
+                    };
+                }
+            }
+        }
+
+        private static void ConfigureDefaultQueryProvider(IQueryConfigurator<DataContext> options)
+        {
+            options.Configure<Invoice>(i =>
+            {
+                i.Map("Id", e => e.InvoiceId);
+                i.Map("Paid", e => e.Paid);
+                i.Map("Amount", e => e.Amount);
+                i.Map("Comment", e => e.Comment);
+                i.Map("Cancelled", e => e.Cancelled);
+                i.Map("Discount", e => e.Discount);
+            });
+        }
+    }
+}
---
+++ src/Spectre.Query.Tests/Infrastructure/TestQueryRunnerResultExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Spectre.Query.Tests.Infrastructure
+{
+    public static class TestQueryRunnerResultExtensions
+    {
+        public static void ShouldContainEntities(this TestQueryRunnerResult result, params int[] expected)
+        {
+            // Expected number of items?
+            if (expected.Length != result.Result.Count)
+            {
+                throw new InvalidOperationException($"Expected {expected.Length} items but got {result.Result.Count}.");
+            }
+
+            // All expected items in collection?
+            foreach (var item in expected)
+            {
+                if (result.Result.Find(x => x.InvoiceId == item) == null)
+                {
+                    throw new InvalidOperationException($"Item #{item} is not in result collection.");
+                }
+            }
+        }
+    }
+}
